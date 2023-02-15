3a97ee49a4ac4020a9e72fc1afea23abe68e0be9
Lipon Roy <liponbdbd@gmail.com>
2023-02-15 13:48:49 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<span className=' text-4xl font-bold text-green-700'>
+				I Love <span className=' text-red-600'>Bangladesh</span> !
+			</span>
+		</div>
+	);
+}
+
+export default App;
