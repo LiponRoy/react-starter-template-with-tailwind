35523cfc91a5ed0b9117c1e2b95c88836888eaf8
Lipon Roy <liponbdbd@gmail.com>
2023-05-29 09:19:24 +0600

    project start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import Hero from './components/hero/Hero';
 
 function App() {
 	return (
 		<>
 			<Navbar></Navbar>
-			<Hero></Hero>
 		</>
 	);
 }
